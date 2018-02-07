d83f37613075b2041bac178bda26936755bdd18b
dutchpool <33056789+dutchpool@users.noreply.github.com>
2018-02-07 22:35:24 +0100

    Delete printing.py

--- src/console/printing.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'dutch_pool'
-
-
-import datetime
-
-
-def __print(message):
-    time_string = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S: ")
-    print(time_string + message)
