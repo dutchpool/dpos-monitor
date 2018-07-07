4a9b6b6d432303ef765b95d9f496f22741c6606b
dutchpool <33056789+dutchpool@users.noreply.github.com>
2018-07-07 21:44:18 +0200

    Added support for very large telegram messages

--- src/telegram.py
+++ src/telegram.py
@@ -3,7 +3,6 @@ from urllib.parse import urlparse
 import requests
 from printing import __print
 
-
 __author__ = 'dutch_pool'
 
 telegram_conf = {}
@@ -20,11 +19,20 @@ def __send_telegram_message(message):
         if not telegram_conf["use_telegram"]:
             return
 
-        uri = urlparse(
-            'https://api.telegram.org/bot' + telegram_conf["bot_key"] + '/sendMessage?chat_id=' + telegram_conf[
-                "chat_id"] + '&text=' + message
-        ).geturl()
-        requests.get(uri)
+        if len(message) < 2000:
+            uri = urlparse(
+                'https://api.telegram.org/bot' + telegram_conf["bot_key"] + '/sendMessage?chat_id=' + telegram_conf[
+                    "chat_id"] + '&text=' + message
+            ).geturl()
+            requests.get(uri)
+        else:
+            chunks = int(len(message) / 2000) + 1
+            for i in range(0, chunks):
+                uri = urlparse(
+                    'https://api.telegram.org/bot' + telegram_conf["bot_key"] + '/sendMessage?chat_id=' + telegram_conf[
+                        "chat_id"] + '&text=' + message[i * chunks: (i + 1) * chunks]
+                ).geturl()
+                requests.get(uri)
     except Exception as e:
         __print('Unable to send telegram message.')
         print(e)
